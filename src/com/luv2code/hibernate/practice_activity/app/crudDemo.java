4ff84778b67efd3bf1db80c2fd22b034be95ce1a
Patryk Glegoła <patryk.glegola@gmail.com>
2018-03-16 20:33:17 +0100

    Practice Activity #8: Hibernate Development - retrieve objects by primary key.

--- src/com/luv2code/hibernate/practice_activity/app/crudDemo.java
+++ src/com/luv2code/hibernate/practice_activity/app/crudDemo.java
@@ -7,18 +7,19 @@ import org.hibernate.cfg.Configuration;
 
 public class crudDemo {
 
+    private static Session session;
+
     public static void main(String[] args) {
 
         SessionFactory sessionFactory = new Configuration()
                 .configure("hibernate.cfg.xml")
                 .addAnnotatedClass(Employee.class)
                 .buildSessionFactory();
-
-        Session session = sessionFactory.getCurrentSession();
+        session = sessionFactory.getCurrentSession();
         try {
+            //createAndSaveSomeObject();
+            displayObjectWithId(1);
 
-            createAndSaveSomeObject(session);
-            
         }
         finally {
             sessionFactory.close();
@@ -26,13 +27,20 @@ public class crudDemo {
 
     }
 
-    private static void createAndSaveSomeObject(Session session) {
+    private static void createAndSaveSomeObject() {
         Employee employee1 = new Employee("Adam", "Smith", "Pear");
         Employee employee2 = new Employee("Michael", "Roberts", "Megahard");
 
         session.beginTransaction();
         session.save(employee1);
         session.save(employee2);
+        session.getTransaction().commit();
+    }
+
+    private static void displayObjectWithId(int id) {
+        session.beginTransaction();
+        Employee readEmployee = session.get(Employee.class, id);
+        System.out.println("\nEmployee from database with id " + id + ": \n" + readEmployee);
         session.getTransaction().commit();
     }
 
