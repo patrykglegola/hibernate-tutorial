f716ef1acfa886e42cac597e42250af041b5b959
Patryk Glegoła <patryk.glegola@gmail.com>
2018-03-16 18:18:15 +0100

    Hibernate CRUD Features: Updating Objects.

---
+++ src/com/luv2code/hibernate/demo/UpdateStudentDemo.java
@@ -0,0 +1,55 @@
+package com.luv2code.hibernate.demo;
+
+import com.luv2code.hibernate.demo.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class UpdateStudentDemo {
+
+    public static void main(String[] args) {
+
+        //create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+
+        //create session
+        Session session = factory.getCurrentSession();
+
+        try {
+
+            int studentId = 1;
+
+            // get a new session and start transaction
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+
+            //retrieve student based on the id: primary key and update his name
+            System.out.println("\nGetting student with id: " + studentId);
+            Student student = session.get(Student.class, studentId);
+            System.out.println("Updating student...");
+            student.setFirstName("Scooby");
+
+            //commit transaction
+            session.getTransaction().commit();
+
+            // get a new session and start transaction
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+
+            //update email for all students
+            System.out.println("Update email for all students");
+            session.createQuery("update Student set email = 'foo@gmail.com'").executeUpdate();
+
+            //commit transaction
+            session.getTransaction().commit();
+
+            System.out.println("Done!");
+        }
+        finally {
+            factory.close();
+        }
+    }
+}
